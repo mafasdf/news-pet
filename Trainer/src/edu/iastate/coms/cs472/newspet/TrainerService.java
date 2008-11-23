7f970f5981c88782e1339677ab30322be29dc555
Michael.Fulker <Michael.Fulker@e8a98aa2-b1e6-11dd-8d2d-9519526c4844>
2008-11-23 22:17:57 +0000

    Initial import.

---
+++ Trainer/src/edu/iastate/coms/cs472/newspet/TrainerService.java
@@ -0,0 +1,19 @@
+package edu.iastate.coms.cs472.newspet;
+
+/**
+ * @author Michael Fulker
+ */
+public class TrainerService
+{
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO
+		//(just testing jar-referencing and svn)
+		System.out.println((new weka.classifiers.bayes.NaiveBayes()).toString());
+	}
+	
+}
