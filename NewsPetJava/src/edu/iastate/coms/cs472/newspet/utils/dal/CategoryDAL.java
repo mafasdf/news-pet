ffd4ebbe82b2963abc0356cae0854169cca99b97
tyson.d.w <tyson.d.w@e8a98aa2-b1e6-11dd-8d2d-9519526c4844>
2008-12-05 10:56:56 +0000

    Autoformatted.

--- NewsPetJava/src/edu/iastate/coms/cs472/newspet/utils/dal/CategoryDAL.java
+++ NewsPetJava/src/edu/iastate/coms/cs472/newspet/utils/dal/CategoryDAL.java
@@ -12,11 +12,17 @@ import java.sql.SQLException;
 public class CategoryDAL
 {
 	static final String TABLE_NAME = "feed_category";
+	
 	static final String ID_COLUMN = "id";
+	
 	static final String TRASH_COLUMN = "is_trash";
+	
 	static final String USERID_COLUMN = "owner_id";
+	
 	/**
-	 * Retrieves the ID of the trash category for the given user, or null if no such category exists.
+	 * Retrieves the ID of the trash category for the given user, or null if no
+	 * such category exists.
+	 * 
 	 * @param userID
 	 * @return
 	 */
@@ -33,21 +39,18 @@ public class CategoryDAL
 			PreparedStatement getTrashID = conn.prepareStatement(query);
 			java.sql.ResultSet result = getTrashID.executeQuery();
 			
-			if(!result.next())
-				toReturn = null;
-			else
-				toReturn = result.getInt(ID_COLUMN);
+			if(!result.next()) toReturn = null;
+			else toReturn = result.getInt(ID_COLUMN);
 			
 			result.close();
 			getTrashID.close();
 			//TODO: check this in other DALs
-			if(!conn.getAutoCommit())
-				conn.commit();
+			if(!conn.getAutoCommit()) conn.commit();
 			conn.close();
 		}
 		catch(SQLException e)
 		{
-			throw new RuntimeException("Could not retrieve trash category for user:"+userID, e);
+			throw new RuntimeException("Could not retrieve trash category for user:" + userID, e);
 		}
 		
 		return toReturn;
