ec342db0e814a41209b3300d6c2598443898df1e
tyson.d.w <tyson.d.w@e8a98aa2-b1e6-11dd-8d2d-9519526c4844>
2008-12-05 10:49:18 +0000

    Autoformatted.

--- NewsPetJava/src/edu/iastate/coms/cs472/newspet/trainer/TrainingItem.java
+++ NewsPetJava/src/edu/iastate/coms/cs472/newspet/trainer/TrainingItem.java
@@ -3,18 +3,20 @@ package edu.iastate.coms.cs472.newspet.trainer;
 import cc.mallet.types.Instance;
 
 public class TrainingItem
-{	
+{
 	private int classifierID;
+	
 	private Instance data;
+	
 	public int getClassifierID()
 	{
 		return classifierID;
 	}
+	
 	public Instance getData()
 	{
 		return data;
 	}
-	
 	
 	public TrainingItem(int classifierID, Instance data)
 	{
@@ -24,6 +26,6 @@ public class TrainingItem
 	
 	public TrainingItem(int classifierID, int targetCategory, String documentData)
 	{
-		this(classifierID, new Instance(documentData, targetCategory, null,null));
+		this(classifierID, new Instance(documentData, targetCategory, null, null));
 	}
 }
