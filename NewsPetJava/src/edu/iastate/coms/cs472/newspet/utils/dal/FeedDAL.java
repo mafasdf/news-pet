c8beef1b0a3b124d4661da3d4aadadf18c160778
tyson.d.w <tyson.d.w@e8a98aa2-b1e6-11dd-8d2d-9519526c4844>
2008-12-05 11:04:46 +0000

    Forgot to have logic depend on the input parameter.

--- NewsPetJava/src/edu/iastate/coms/cs472/newspet/utils/dal/FeedDAL.java
+++ NewsPetJava/src/edu/iastate/coms/cs472/newspet/utils/dal/FeedDAL.java
@@ -5,10 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 import java.util.List;
-import java.util.TimeZone;
 
 import edu.iastate.coms.cs472.newspet.utils.Feed;
 
@@ -20,30 +17,34 @@ import edu.iastate.coms.cs472.newspet.utils.Feed;
 public class FeedDAL
 {
 	static final String ID_COLUMN = "id";
+	
 	static final String URL_COLUMN = "url";
+	
 	static final String USERID_COLUMN = "subscriber_id";
+	
 	static final String LASTCRAWLED_COLUMN = "last_crawled";
+	
 	static final String FEED_TABLE = "feed_feeditem";
 	
 	/**
-	 * Gets feed objects with "lastCrawled" timestamps older than the given cutoff time. 
-	 * @param cutoff Cutoff time in UTC form.
+	 * Gets feed objects with "lastCrawled" timestamps older than the given
+	 * cutoff time.
+	 * 
+	 * @param cutoff
+	 *        Cutoff time in UTC form.
 	 * @return
 	 */
 	public static List<Feed> getFeedsOlderThan(java.util.Date cutoff)
 	{
 		List<Feed> toReturn = new ArrayList<Feed>();
 		
-		
 		Connection conn = ConnectionConfig.createConnection();
 		
-		String query = "SELECT " + ID_COLUMN + " " + URL_COLUMN + " " + USERID_COLUMN + " " + LASTCRAWLED_COLUMN + " FROM " + FEED_TABLE + " WHERE "
-				+ LASTCRAWLED_COLUMN + " < ? ORDER BY " + LASTCRAWLED_COLUMN + " ASC;";
+		String query = "SELECT " + ID_COLUMN + " " + URL_COLUMN + " " + USERID_COLUMN + " " + LASTCRAWLED_COLUMN + " FROM " + FEED_TABLE + " WHERE " + LASTCRAWLED_COLUMN + " < ? ORDER BY " + LASTCRAWLED_COLUMN + " ASC;";
 		try
 		{
 			PreparedStatement getFeeds = conn.prepareStatement(query);
-			long currentDateMillis = System.currentTimeMillis();
-			getFeeds.setDate(1, new java.sql.Date(currentDateMillis));
+			getFeeds.setDate(1, new java.sql.Date(cutoff.getTime()));
 			ResultSet feedResults = getFeeds.executeQuery();
 			
 			while(feedResults.next())
