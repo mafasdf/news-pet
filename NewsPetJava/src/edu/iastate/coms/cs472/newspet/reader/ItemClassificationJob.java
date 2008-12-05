3bff81d79b9e098af725a06a0b7e3c49c3628135
tyson.d.w <tyson.d.w@e8a98aa2-b1e6-11dd-8d2d-9519526c4844>
2008-12-05 10:25:27 +0000

    Autoformatted.

--- NewsPetJava/src/edu/iastate/coms/cs472/newspet/reader/ItemClassificationJob.java
+++ NewsPetJava/src/edu/iastate/coms/cs472/newspet/reader/ItemClassificationJob.java
@@ -1,10 +1,9 @@
 package edu.iastate.coms.cs472.newspet.reader;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import cc.mallet.classify.Classification;
 import cc.mallet.classify.Classifier;
-import cc.mallet.classify.NaiveBayes;
 import cc.mallet.types.Label;
 import cc.mallet.types.LabelVector;
 import de.nava.informa.core.ItemIF;
@@ -13,7 +12,6 @@ import edu.iastate.coms.cs472.newspet.utils.Pair;
 import edu.iastate.coms.cs472.newspet.utils.dal.CategoryDAL;
 import edu.iastate.coms.cs472.newspet.utils.dal.DatabaseAccessLayer;
 import edu.iastate.coms.cs472.newspet.utils.dal.FeedItemDAL;
-
 
 public class ItemClassificationJob implements Runnable
 {
@@ -34,8 +32,7 @@ public class ItemClassificationJob implements Runnable
 		this.feed = feed;
 		this.feedItem = feedItem;
 	}
-
-
+	
 	/**
 	 * <p>
 	 * Classifies the given feed item and persists it with the corresponding
@@ -48,30 +45,29 @@ public class ItemClassificationJob implements Runnable
 	public void run()
 	{
 		//check if this item already exists for this user (using URL as an equivalence comparison), and abort if so
-		if(FeedItemDAL.existsURL(feedItem.getLink().toString(), feed.getUserId()))
-			return;
+		if(FeedItemDAL.existsURL(feedItem.getLink().toString(), feed.getUserId())) return;
 		
 		Classifier classifier = DatabaseAccessLayer.getClassifier(feed.getUserId());
 		int trashCategory = CategoryDAL.getTrashCategoryIDForUser(feed.getUserId());
 		
 		//extract out all textual information
 		StringBuilder text = new StringBuilder();
-		if(feedItem.getCreator()!=null)
+		if(feedItem.getCreator() != null)
 		{
 			text.append(feedItem.getCreator());
 			text.append(" ");
 		}
-		if(feedItem.getDescription()!=null)
+		if(feedItem.getDescription() != null)
 		{
 			text.append(feedItem.getDescription());
 			text.append(" ");
 		}
-		if(feedItem.getSubject()!=null)
+		if(feedItem.getSubject() != null)
 		{
 			text.append(feedItem.getSubject());
 			text.append(" ");
 		}
-		if(feedItem.getTitle()!=null)
+		if(feedItem.getTitle() != null)
 		{
 			text.append(feedItem.getTitle());
 			text.append(" ");
@@ -81,54 +77,50 @@ public class ItemClassificationJob implements Runnable
 		String bareText = text.toString().replaceAll("<[^<>]*>", " ");
 		
 		LabelVector classificationResultVector = classifier.classify(bareText).getLabelVector();
-		List<Pair<Integer,Double>> categoryProbabilities = getClassificationValues(classificationResultVector);
+		List<Pair<Integer, Double>> categoryProbabilities = getClassificationValues(classificationResultVector);
 		
 		//get maximum probability category
 		Pair<Integer, Double> bestCategory = findBestCategory(categoryProbabilities);
-		
 		
 		int bestCategoryID;
 		//check if probability scaled by the number of categories is at least some cutoff value
 		//(If the probability is too close to 1/numCategories, trash is the chosen category)  
 		if(bestCategory.getB().doubleValue() * categoryProbabilities.size() > PROBABILITY_SIGNIFICANCE_RATIO)
 		{
-			bestCategoryID=bestCategory.getA().intValue();
+			bestCategoryID = bestCategory.getA().intValue();
 		}
 		else
 		{
-			bestCategoryID=trashCategory;
+			bestCategoryID = trashCategory;
 		}
 		
-		//persist a new feeditem in the proper category
+		//persist a new FeedItem in the proper category
 		FeedItemDAL.saveNewFeedItem(feedItem.getTitle(), feedItem.getCreator(), feedItem.getDescription(), feedItem.getLink().toString(), bestCategory.getA(), feed.getId(), feed.getUserId());
 	}
-
-
+	
 	public static List<Pair<Integer, Double>> getClassificationValues(LabelVector classificationResultVector)
 	{
 		double[] probabilities = classificationResultVector.getValues();
-	
+		
 		List<Pair<Integer, Double>> toReturn = new ArrayList<Pair<Integer, Double>>(probabilities.length);
 		
-		for(int i=0;i<probabilities.length;i++)
+		for(int i = 0; i < probabilities.length; i++)
 		{
 			Label label = classificationResultVector.labelAtLocation(i);
-			Integer categoryID = (Integer) label.getEntry(); 
+			Integer categoryID = (Integer) label.getEntry();
 			Pair<Integer, Double> toAdd = new Pair<Integer, Double>(categoryID, Double.valueOf(probabilities[i]));
 			toReturn.add(toAdd);
 		}
 		
 		return toReturn;
 	}
-
 	
 	private Pair<Integer, Double> findBestCategory(List<Pair<Integer, Double>> categoryProbabilities)
 	{
 		Pair<Integer, Double> best = null;
 		for(Pair<Integer, Double> candidate : categoryProbabilities)
 		{
-			if(best==null || candidate.getB().doubleValue() > best.getB().doubleValue())
-				best=candidate;
+			if(best == null || candidate.getB().doubleValue() > best.getB().doubleValue()) best = candidate;
 		}
 		return best;
 	}
