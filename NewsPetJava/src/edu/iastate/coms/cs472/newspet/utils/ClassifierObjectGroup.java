2389fa28eae8adbf43f1e1354946648ec3e60e9f
tyson.d.w <tyson.d.w@e8a98aa2-b1e6-11dd-8d2d-9519526c4844>
2008-12-05 10:50:07 +0000

    Autoformatted.

--- NewsPetJava/src/edu/iastate/coms/cs472/newspet/utils/ClassifierObjectGroup.java
+++ NewsPetJava/src/edu/iastate/coms/cs472/newspet/utils/ClassifierObjectGroup.java
@@ -7,8 +7,11 @@ import cc.mallet.classify.NaiveBayesTrainer;
 import cc.mallet.pipe.Pipe;
 
 /**
- * The 3 classifier-related objects that need to be persisted are: ClassifierTrainer, Classifier, and Pipe.
- * Since these share certain objects as member variables, it is more efficient to serialize them in a group, such as in this object.   
+ * The 3 classifier-related objects that need to be persisted are:
+ * ClassifierTrainer, Classifier, and Pipe. Since these share certain objects as
+ * member variables, it is more efficient to serialize them in a group, such as
+ * in this object.
+ * 
  * @author Michael Fulker
  */
 public class ClassifierObjectGroup implements Serializable
@@ -16,8 +19,11 @@ public class ClassifierObjectGroup implements Serializable
 	private static final long serialVersionUID = 1L;
 	
 	private NaiveBayes classifier;
+	
 	private NaiveBayesTrainer trainer;
+	
 	private Pipe pipe;
+	
 	/**
 	 * @param classifier
 	 * @param trainer
@@ -34,14 +40,14 @@ public class ClassifierObjectGroup implements Serializable
 	{
 		return classifier;
 	}
+	
 	public NaiveBayesTrainer getTrainer()
 	{
 		return trainer;
 	}
+	
 	public Pipe getPipe()
 	{
 		return pipe;
 	}
-	
-	
 }
