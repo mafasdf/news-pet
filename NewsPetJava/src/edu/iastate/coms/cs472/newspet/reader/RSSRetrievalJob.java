08a9f7e41b18f656b9d518dbe1541107d0d34336
tyson.d.w <tyson.d.w@e8a98aa2-b1e6-11dd-8d2d-9519526c4844>
2008-12-05 10:30:10 +0000

    Removed an unused import. Autoformatted.

--- NewsPetJava/src/edu/iastate/coms/cs472/newspet/reader/RSSRetrievalJob.java
+++ NewsPetJava/src/edu/iastate/coms/cs472/newspet/reader/RSSRetrievalJob.java
@@ -1,11 +1,9 @@
 package edu.iastate.coms.cs472.newspet.reader;
 
-
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Collection;
-import java.util.List;
 
 import de.nava.informa.core.ChannelIF;
 import de.nava.informa.core.ParseException;
@@ -14,41 +12,37 @@ import de.nava.informa.parsers.FeedParser;
 import edu.iastate.coms.cs472.newspet.utils.Feed;
 import edu.iastate.coms.cs472.newspet.utils.Pair;
 
-
 /**
- * Job that retrieves 
+ * Job that retrieves
+ * 
  * @author Michael Fulker
- *
  */
 public class RSSRetrievalJob implements Runnable
 {
 	/**
-	 * Shared list of results.
-	 * Must be added to in a synchronized manner.
+	 * Shared list of results. Must be added to in a synchronized manner.
 	 */
-	private Collection<Pair<Feed,ChannelIF>> resultStorage;
+	private Collection<Pair<Feed, ChannelIF>> resultStorage;
 	
 	/**
 	 * Feed to read from.
 	 */
 	private Feed feed;
 	
-	
-	
-	
 	/**
 	 * Creates a new RSS Retrieval job.
+	 * 
 	 * @param resultStorage
-	 * The location to store retrieved channel data.
+	 *        The location to store retrieved channel data.
 	 * @param feed
-	 * Feed to read from.
+	 *        Feed to read from.
 	 */
-	public RSSRetrievalJob(Collection<Pair<Feed,ChannelIF>> resultStorage, Feed feed)
+	public RSSRetrievalJob(Collection<Pair<Feed, ChannelIF>> resultStorage, Feed feed)
 	{
 		this.resultStorage = resultStorage;
 		this.feed = feed;
 	}
-
+	
 	/**
 	 * Retrieves channel data and stores in resultStorage.
 	 */
@@ -58,7 +52,7 @@ public class RSSRetrievalJob implements Runnable
 		ChannelIF channel;
 		try
 		{
-			url =  new URL(feed.getUrl());
+			url = new URL(feed.getUrl());
 		}
 		catch(MalformedURLException e)
 		{
@@ -69,22 +63,21 @@ public class RSSRetrievalJob implements Runnable
 		{
 			channel = FeedParser.parse(new ChannelBuilder(), url);
 		}
-		catch(IOException e1)
+		catch(IOException e)
 		{
-			e1.printStackTrace();
+			e.printStackTrace();
 			return;
 		}
-		catch(ParseException e1)
+		catch(ParseException e)
 		{
-			e1.printStackTrace();
+			e.printStackTrace();
 			return;
 		}
 		
-		Pair<Feed,ChannelIF> toAdd = new Pair<Feed,ChannelIF>(feed, channel);
+		Pair<Feed, ChannelIF> toAdd = new Pair<Feed, ChannelIF>(feed, channel);
 		synchronized(resultStorage)
 		{
 			resultStorage.add(toAdd);
 		}
 	}
-	
 }
