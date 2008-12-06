8a644b92479ac58a5246f0737461975a12feb6fa
thauber <thauber@e8a98aa2-b1e6-11dd-8d2d-9519526c4844>
2008-12-06 20:39:27 +0000

    removed old newspet folder

--- newspet/feed/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from feed.models import *
-
-admin.site.register(Feed)
-admin.site.register(FeedItem)
-admin.site.register(Category)
-admin.site.register(TrainingSet)
--- newspet/sql/feed.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE TABLE Classifier
-(
-ID int NOT NULL,
-serializedClassifier longblob,
-CONSTRAINT PRIMARY KEY USING HASH (ID),
-CONSTRAINT FOREIGN KEY (ID) REFERENCES auth_user(ID)
-);
